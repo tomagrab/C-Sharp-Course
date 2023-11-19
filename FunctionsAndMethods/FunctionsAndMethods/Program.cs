d3eef6e0afcfa1db0af22c457d3384cb532a74ea
Tommy Boi <tomaarongrabowski@gmail.com>
2023-11-19 10:54:57 -0500

    Added notes to FunctionsAndMethods solution

---
+++ FunctionsAndMethods/FunctionsAndMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FunctionsAndMethods
+{
+    class Program
+    {
+        // A method is a code block that contains a series of statements.
+        // A program causes the statements to be executed by calling the method and specifying any required method arguments.
+        // In C#, every executed instruction is performed in the context of a method.
+        // The main method is the entry point for every C# application and it is called by the common language runtime (CLR) when the program is started.
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
