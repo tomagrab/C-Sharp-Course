756f7731a6ca6fc33c9c8ed22a60f8d5a611b250
Tommy Boi <tomaarongrabowski@gmail.com>
2023-11-20 04:54:26 -0500

    Started 'Intro to Decision Making'

---
+++ DecisionMakingIntro/DecisionMakingIntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DecisionMakingIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int temperature;
+
+            Console.Write("Enter the temperature: ");
+            input = Console.ReadLine();
+
+            while (!int.TryParse(input, out temperature))
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+                Console.Write("Enter the temperature: ");
+                input = Console.ReadLine();
+            }
+
+            if (temperature <= 10)
+            {
+                Console.WriteLine("It's cold!");
+            }
+            else if (temperature > 20)
+            {
+                Console.WriteLine("It's mild!");
+            }
+            else
+            {
+                Console.WriteLine("It's warm!");
+            }
+        }
+    }
+}
---
+++ TryCatchFinally/TryCatchFinally/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Invalid format");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Number too large");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Error: Argument is null");
+            }
+            finally
+            {
+                Console.WriteLine("This is called anyways");
+            }
+        }
+    }
+}
