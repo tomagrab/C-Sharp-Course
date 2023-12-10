fb56d9dc57d9d3cc2fa6500501a53f24f31a280f
Tommy Boi <tomaarongrabowski@gmail.com>
2023-12-10 12:37:03 -0500

    Updated InheritanceDemo

--- InheritanceDemo/Program.cs
+++ InheritanceDemo/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace InheritanceDemo
+﻿using InheritanceDemo.Models;
+
+namespace InheritanceDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Post post1 = new Post("Thanks for the birthday wishes", true, "Derek Bananas");
 
+            Console.WriteLine(post1.ToString());
         }
     }
 }
