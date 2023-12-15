838166cabcefa7cdccc4acac05a0fd3f377b639d
Tommy Grabowski <tommy.grabowski@velocitor.com>
2023-12-15 08:00:20 -0500

    Complete Enums lesson

---
+++ Enums/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Enus
+{
+    enum Day { Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day fr = Day.Viernes;
+            Day sun = Day.Domingo;
+        }
+    }
+}
