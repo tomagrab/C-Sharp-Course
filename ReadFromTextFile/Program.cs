38ebca8128629548ea1c654836a39e0111f7b0fb
Tommy Grabowski <tommy.grabowski@velocitor.com>
2023-12-15 07:42:14 -0500

    Completed Structs lesson

---
+++ ReadFromTextFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        /* 
+            * Create a class Program with the Main method with code that will read from the input.txt line by line;
+            * If one of the lines contains "split" find it;
+            * And after, split it (use the split method from the string class)
+            * Take the return from the split and write the element with index 4 into the output.txt ;
+            * You have to add a space between each element to make it readable;
+         */
+
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            string output = "";
+            foreach (string line in lines)
+            {
+                if (line.Contains("split"))
+                {
+                    string[] split = line.Split(" ");
+                    output += split[4] + " ";
+                }
+            }
+            File.WriteAllText("output.txt", output);
+        }
+    }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Structs
+{
+    struct Game
+    {
+        public string name;
+        public string developer;
+        public double rating;
+        public string releaseDate;
+        public Game(string name, string developer, double rating, string releaseDate)
+        {
+            this.name = name;
+            this.developer = developer;
+            this.rating = rating;
+            this.releaseDate = releaseDate;
+        }
+        public override string ToString()
+        {
+            return $"Name: {name}\nDeveloper: {developer}\nRating: {rating}\nRelease Date: {releaseDate}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game1 = new Game("The Legend of Zelda: Breath of the Wild", "Nintendo", 9.5, "March 3, 2017");
+
+            Console.WriteLine(game1.ToString());
+        }
+    }
+}
